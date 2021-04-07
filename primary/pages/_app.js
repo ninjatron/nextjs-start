6214a3ae7fbac04694b9e360a4878752b4346a0f
aysad <aysad@umich.edu>
2021-04-07 04:51:21 -0400

    aa

--- primary/pages/_app.js
+++ primary/pages/_app.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -27,20 +27,11 @@ const GlobalStyle = createGlobalStyle`
     width: 30px;
   }
 `
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
-
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
